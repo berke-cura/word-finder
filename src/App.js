63415a15568c70a653eecf6723561960d00f84cc
berke-cura <berkecura@protonmail.com>
2022-04-03 01:25:09 +0300

    ----

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   NavLink,
   HashRouter,
   Routes,
-  Switch
 
 } from "react-router-dom";
 import "./index.css";
@@ -17,30 +16,21 @@ class Main extends Component {
   render() {
     return (
       <HashRouter>
-        <div>
-          
-      
-          <h1>Simple SPA</h1>
+        <div>                
+          <h1>Word Finder</h1>
           <ul className="header">
           <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/stuff">Stuff</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+          <li><NavLink to="/stuff">Stuff</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
-          <div>
-       
+          <div>       
       </div>
-          <div className="content">
-          
-  
+          <div className="content">            
           <Routes>
-          <Route path="/" element={<Home/>}></Route>
+            <Route path="/" element={<Home/>}></Route>
             <Route path="/stuff" element={<Stuff/>}></Route>
             <Route path="/contact" element={<Contact/>}></Route>
-
-
-            </Routes>
-      
-
+          </Routes>      
           </div>
         </div>
         </HashRouter>
--- src/home.js
+++ src/home.js
@@ -1,21 +1,48 @@
 import React, { Component } from "react";
+import Box from '@mui/material/Box';
+
+import { TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+
+
+
  
 class Home extends Component {
+    
   render() {
     return (
       <div>
         <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} id="filled-basic" label="Letter Count" margin="normal" variant="outlined"/>
+    
+        <TextField id="filled-basic" label="Existing Letters" variant="outlined"/>
+        <TextField id="filled-basic" label="Not Existing Letters" variant="outlined"/>
+        <TextField id="filled-basic" label="Letter Count" variant="outlined"/>
+        <Button variant="outlined">Submit</Button>
+
+        
       </div>
+     
+    </Box>
+
+
+       
+      </div>
+      
+      
     );
+    
   }
+  
 }
  
 export default Home;
