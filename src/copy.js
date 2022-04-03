ea3a06bd74620f375cc9af959743f327af47b02e
berke-cura <berkecura@protonmail.com>
2022-04-03 21:45:52 +0300

    css updates

--- src/copy.js
+++ src/copy.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
 
 import ('./index.css')
+const data = ["ahmet", "mehmet", "aslı","ahmet", "mehmet", "aslı","ahmet", "mehmet", "aslı","ahmet", "mehmet", "aslı","ahmet", "mehmet", "aslı"]
+const result = data.join(', ')
 
 class Home extends Component {
     
-    render() {
+  render() {
         return (
-        <form onSubmit={this.handleSubmit}>
             <div className="grid-containerA">
                 <div className="grid-item item1A"><label>LETTER COUNT<input type="text"  name="Letter Count"/></label></div>
                 <div className="grid-item item2A"><label>EXISTING LETTERS<input type="text"  name="Existing Letters"/></label></div>
                 <div className="grid-item item3A"><label>NOT EXISTING LETTERS<input type="text"  name="Not Existing Letters"/></label></div>  
                 <div className="grid-item item4A"><label>LETTER AND PLACE<input type="text"  name="Letter and Place"/></label></div>
-                <div className="grid-item item5A"><label>RESULTS<input type="text"  name="Results" /></label></div>
+                <h1>RESULTS</h1>
+                <textarea disabled={true} >{result}</textarea>
                 <button type="submit">SEARCH</button>
             </div>
-      </form>
 
-    );
+        );
   }
 }
 
